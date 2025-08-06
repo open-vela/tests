90b2949ef5623ab6bae8382d2e283b355971702a
liyanqi5 <liyqi_1101@163.com>
2025-08-06 20:37:43 +0800

    Update test_cmocka.py

--- scripts/script/test_framework/test_cmocka.py
+++ scripts/script/test_framework/test_cmocka.py
@@ -26,7 +26,7 @@ import os
 
 import pytest
 
-pytestmark = [pytest.mark.rv_virt]
+pytestmark = [pytest.mark.common, pytest.mark.rv_virt, pytest.mark.goldfish_armeabi_v7a_ap]
 
 cmocka_list_start = "cmocka_list_start"
 cmocka_list_end = "cmocka_list_end"
